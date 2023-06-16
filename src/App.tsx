08919a374232a8ca7f40df01fa226a373ddbdf99
Админ <admin@MacBook-Pro-Admin.local>
2023-06-16 08:22:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+
+  const [speed, setSpeed] = useState(7);
+  const [tileCount, setTileCount] = useState(20);
+  const [tileSize, setTileSize] = useState(0)
+  const [headX, setHeadX] = useState(10);
+  const [headY, setHeadY] = useState(10);
+  const [xVelocity, setXVelocity] = useState(0)
+  const [yVelocity, setYVelocity] = useState(0)
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const ctx = canvasRef.current?.getContext('2d')
+
+  useEffect(() => {
+    setTileSize(canvasRef.current!.width / tileCount - 2)
+  }, [canvasRef, tileCount])
+
+  function clearScreen() {
+    if (ctx) {
+      ctx!.fillStyle = "black";
+      ctx?.fillRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
+    }
+  }
+  function drawSnake() {
+    if (ctx) {
+      ctx!.fillStyle = 'orange'
+      ctx?.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
+    }
+  }
+  // TODO: it's a game loop
+  function drawGame() {
+    clearScreen()
+    drawSnake()
+
+    // setTimeout(drawGame, 1000 / speed)
+  }
+
+  drawGame()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <canvas ref={canvasRef} width={400} height={400} className="game"></canvas>
+    </>
   );
 }
 
